d04dc29c31cb0334816307eb5d5d177016de7c3a
OscarHeMe <oscar_hdez_m@hotmail.com>
2020-07-19 15:32:03 -0500

    added structure for bulk_insert

--- api/products/views.py
+++ api/products/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    reponse = {
+        'message': 'Product'
+    }
+    return JsonResponse(reponse)
+
+
+def bulk_insert(request):
+    try:
+        products = request.POST['products']
+        return JsonResponse(products, status=201)
+    except Exception as e:
+        error_msg = str(e)
+        reponse = {
+            'error': error_msg
+        }
+        return JsonResponse(reponse, status=400)
+
+
