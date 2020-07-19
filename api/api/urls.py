9fbda25bfc26d870d991017883b2f69f6b538d47
OscarHeMe <oscar_hdez_m@hotmail.com>
2020-07-19 15:19:40 -0500

    renamed files to meet project stucture

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,4 +1,4 @@
-"""products URL Configuration
+"""api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', include('product.urls'))
+    path('products/', include('products.urls'))
 ]
