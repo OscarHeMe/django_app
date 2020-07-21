55098b3dd68f5b48f13e315d636b269faf34c8ce
OscarHeMe <oscar_hdez_m@hotmail.com>
2020-07-20 22:35:51 -0500

    improved validation logs

--- api/products/models.py
+++ api/products/models.py
@@ -22,14 +22,23 @@ class Product(models.Model):
 
     def validate(self):
         error_fileds = []
-        if not not isinstance(self.name, str) or len(self.name) < 3:
-            error_fileds.append('name')
-        if not isinstance(self.value, float) or self.value <= 0 or self.value > 99999.9:
-            error_fileds.append('value')
-        if self.discount_value and (not not isinstance(self.discount_value, float) or self.value < self.discount_value):
-            error_fileds.append('discount_value')
-        if not isinstance(self.stock, int) or self.stock <= -1:
-            error_fileds.append('stock')
+        if not isinstance(self.name, str):
+            error_fileds.append('name must be a string')
+        elif len(self.name) < 3 or len(self.name) > 55:
+            error_fileds.append('name lenght must be greater than 2 and lower than 56')
+        if not isinstance(self.value, float):
+            error_fileds.append('value has to be float')
+        elif self.value <= 0 or self.value > 99999.9:
+            error_fileds.append('value must be greater than 0 and lower than 99999.9')
+        if self.discount_value:
+            if not isinstance(self.discount_value, float):
+                error_fileds.append('discount_value has to be float')
+            elif self.value < self.discount_value:
+                error_fileds.append('discount_value has to be lower than value')
+        if not isinstance(self.stock, int):
+            error_fileds.append('stock must be integer')
+        elif self.stock <= -1:
+            error_fileds.append('stock must be greater than -1')            
         if error_fileds:
             return False, error_fileds
         return True, error_fileds
